0643faef8011fcbe026e626e01d3855749330294
Melina Estefanía <meapilon@gmail.com>
2022-02-15 10:45:39 -0300

    add birthdayCakeCandles

---
+++ HackerRank/birthdayCakeCandles.js
@@ -0,0 +1,10 @@
+function birthdayCakeCandles(candles) {
+  // Write your code here
+
+  let max = Math.max(...candles);
+  let count = 0;
+  for (let i = 0; i < candles.length; i++) {
+    if (candles[i] === max) count += 1;
+  }
+  return count;
+}
